e1a9e3e9412b8cea38c370f8e69e67d96c55e070
ajaymkatte <ajaymkatte95@gmail.com>
2017-07-17 19:16:52 +0000

    added credit.c; yet to be commented

--- morecomf/credit.c
+++ morecomf/credit.c
@@ -1,33 +1,47 @@
 #include<stdio.h>
 #include<cs50.h>
+int splitx(long long ccnum)
+{
+    int junk=ccnum%10;
+    return junk;
+}
 int main(void)
 {
-    printf("Number: ");
-    long long cc;
-    cc=get_long_long();
-    int a=1;
-    long long divide=10;
-    int sum1=0, junk, sum2=0;
-    while(a<=16)
+    printf("Number:\t");
+    long long ccnum=get_long_long();
+    int ultimate=0;
+    int n=0, decimal=0;
+    int sum=0, sum1=0, sum2=0;
+    do
     {
-        if(a%2==0)
-            sum1=sum1+cc%divide;
+        decimal=ultimate;
+        ultimate=ccnum;
+        int junk=splitx(ccnum);
+        if(ccnum<0)
+        {
+            
+        }
+        if(n%2==0)
+            sum1=sum1+junk;
         else
         {
-            junk=cc%divide;
-            junk*=2;
-            if(junk>=10)
-            {
-                int quo,rem;
-                quo=junk/10;
-                rem=junk%10;
-                junk=quo+rem;
-            }
+            int junks=junk*2;
+            sum2=sum2+(junks%10)+(junks/10);
         }
-        sum2=sum2+junk;
-        divide*=10;
-        a++;
+        ccnum/=10;
+        n++;
     }
-    int sum=sum1+sum2;
-    printf("%d",sum);
+    while(ccnum>1);
+    sum=sum1+sum2;
+    if(sum%10==0)
+    {
+        if(decimal==34||decimal==37)
+            printf("AMEX\n");
+        if(ultimate==4)
+            printf("VISA\n");
+        if(decimal==51||decimal==52||decimal==53||decimal==54||decimal==55)
+            printf("MASTERCARD\n");
+    }
+    else
+        printf("INVALID\n");
 }
