0442cbb35dd828035670403fe9e7339633eaa36c
ajaymkatte <ajaymkatte95@gmail.com>
2017-07-18 09:15:15 +0000

    added initials.c; both normal and hacker editions. comments are yet to be added

---
+++ morecomf/initials.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+int main(void)
+{
+    string name=get_string();
+    int length=strlen(name);
+    int e=name[0];
+    if(e!=32)
+    {
+        if(e>=97&&e<=123)
+            printf("%c",e-32);
+        else
+            printf("%c",e);
+    }
+    for(int p=0; p<length; p++)
+    {
+        int c=name[p], d=name[p+1];
+        if(c==32)
+        {
+            if(d!=32)
+            {
+                if(d>=97&&d<=123)
+                    printf("%c",d-32);
+                if(d>=65&&d<=91)
+                    printf("%c",d);
+            }
+        }
+    }
+    printf("\n");
+}
