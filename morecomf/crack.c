74dfaf49ee3d51170e18f0e9b91ab595bf526bc4
ajaymkatte <ajaymkatte95@gmail.com>
2017-07-27 09:33:34 +0000

    cracks code up to five characters long

--- morecomf/crack.c
+++ morecomf/crack.c
@@ -10,7 +10,7 @@ void comparestrings(char* finalhashed, char* final, char* hashed)
 {
     int length=strlen(finalhashed);
     
-    //checks if the strings are the same//
+//checks if the strings are the same//
     
     for(int n=0; n<length; n++)
     {
@@ -32,6 +32,22 @@ void comstrandargv(char* final, char* salt, char* hashed)
     comparestrings(finalhashed, final, hashed);
 }
 
+
+//passes an alphabet as requried, controls both uppercase and lowercase while excluding the rest of the ascii keys//
+
+
+char retrievechar(int x)
+{
+    char alpha;
+    if(x<=25)
+        alpha=65+x;
+    else
+        alpha=65+x+6;
+    return alpha;
+}
+
+//main starts from here//
+
 int main(int argc, string argv[])
 {
     if(argc!=2)
@@ -48,12 +64,105 @@ int main(int argc, string argv[])
     
     //get the pass//
     
-    for(int n=0; n<10; n++)
+    for(int n=0; n<5; n++)
     {
-        for(int p=0; p<26; p++)
+        for(int p=0; p<52; p++)
         {
-            char final[2]={'z', '\0'};
-            comstrandargv(final,salt,hashed);
+            char alpha0 = retrievechar(p);
+            if(n==0)
+            {
+                char final[2] = {alpha0,'\0'};
+                comstrandargv(final,salt,hashed);
+            }
+            
+//if the pass is more than one character long//
+            
+            if(n>0)
+            {
+                
+//if the pass is two characters long//
+                
+                if(n==1)
+                {
+                    for(int q=0; q<52; q++)
+                    {
+                        char alpha1 = retrievechar(q);
+                        char final[3] = {alpha0, alpha1, '\0'};
+                        comstrandargv(final,salt,hashed);
+                    }
+                }
+                
+//ends//
+                
+//if the pass is three characters long//
+                
+                
+                if(n==2)
+                {
+                    for(int q=0; q<52; q++)
+                    {
+                        char alpha1 = retrievechar(q);
+                        for(int r=0; r<52; r++)
+                        {
+                            char alpha2 = retrievechar(r);
+                            char final[4] = {alpha0, alpha1, alpha2, '\0'};
+                            comstrandargv(final,salt,hashed);
+                        }
+                    }
+                }
+                
+//ends//
+                
+//if the pass is four characters long//
+                
+                if(n==3)
+                {
+                    for(int q=0; q<52; q++)
+                    {
+                        char alpha1 = retrievechar(q);
+                        for(int r=0; r<52; r++)
+                        {
+                            char alpha2 = retrievechar(r);
+                            for(int s=0; s<52; s++)
+                            {
+                                char alpha3 = retrievechar(s);
+                                char final[5] = {alpha0, alpha1, alpha2, alpha3, '\0'};
+                                comstrandargv(final,salt,hashed);
+                            }
+                        }
+                    }
+                }
+                
+//ends//
+                
+                
+//if the pass is five charcters long//
+                
+                if(n==4)
+                {
+                    for(int q=0; q<52; q++)
+                    {
+                        char alpha1 = retrievechar(q);
+                        for(int r=0; r<52; r++)
+                        {
+                            char alpha2 = retrievechar(r);
+                            for(int s=0; s<53; s++)
+                            {
+                                char alpha3 = retrievechar(s);
+                                for(int t=0; t<52; t++)
+                                {
+                                    char alpha4 = retrievechar(t);
+                                    char final[6] = {alpha0, alpha1, alpha2, alpha3, alpha4, '\0'};
+                                    comstrandargv(final,salt,hashed);
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+                
+//ends//
+            }
         }
     }
     return 0;
