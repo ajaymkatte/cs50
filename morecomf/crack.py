25fa98279a0c27fc1554ff809e071f28aa8c5ef2
ajaymkatte <ajaymkatte95@gmail.com>
2017-08-26 13:52:13 +0000

    added crack.py

---
+++ morecomf/crack.py
@@ -0,0 +1,36 @@
+# cracks a password of length one for sanity check
+
+# import sys and crypt
+import sys
+import crypt
+
+def main():
+
+    # check if the argv
+    if(len(sys.argv) != 2):
+        print("error")
+        exit(2)
+
+    # get salt
+    salt = sys.argv[1][0], sys.argv[1][1]
+    salt = "".join(salt)
+
+    # interate
+    for i in range(26):
+        password = chr(65 + i)
+        check(password, salt)
+    for i in range(26):
+        password = chr(97 + i)
+        check(password, salt)
+
+    # if all else fails
+    print("no luck")
+
+# check correctness of the password
+def check(password, salt):
+    if sys.argv[1] == crypt.crypt(password, salt):
+        print("{}".format(password))
+        exit(0)
+
+if __name__ == "__main__":
+    main()
