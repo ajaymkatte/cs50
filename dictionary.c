006ade47b024b36b09ef15ddbd4bb25c75f5847c
ajaymkatte <ajaymkatte95@gmail.com>
2017-08-27 11:12:38 +0000

    huge boost in time

--- dictionary.c
+++ dictionary.c
@@ -1,16 +1,26 @@
 /**
- * Implements a dictionary's functionality, no hash table used. I'll be implementing one in the coming days.
+ * Implements a dictionary's functionality using hash table.
  */
 
 #include <stdbool.h>
 
-// included string library
 #include <string.h>
-
+#include <strings.h>
 #include "dictionary.h"
 
 // declare a global hash table
 struct node *ht[24];
+
+// hashtable
+int hashit(char word)
+{
+    int ju;
+    if(word >= 65 && word <= 91)
+        ju = word - 65;
+    else
+        ju = word - 97;
+    return ju;
+}
 
 // destroy(free) the linked list
 void realdestory(struct node *ptr)
@@ -38,62 +48,15 @@ bool destroy(void)
 bool check(const char *word)
 {
 
-    // temp storage
-    int ju;
-    if(word[0] >= 65 && word[0] <= 91)
-        ju = word[0] - 65;
-    else
-        ju = word[0] - 97;
-
-    // temp pointer
-    struct node *ptr = ht[ju];
+    struct node *ptr = ht[hashit(word[0])];
 
     // iterate through the linked list
     while(ptr != NULL)
     {
-        int x = strcmp(ptr -> spell, word);
-
-        // checks if strings are same
-        if(x == 0)
+        if(strcasecmp(ptr -> spell, word) == 0)
             return true;
 
-        // get the next string
-        else if(x < 0)
-            ptr = ptr -> next;
-
-        // apostrophes usage
-        else
-        {
-
-            int len = strlen(ptr -> spell), equals = 0;
-
-            // iterate through the word
-            for(int i = 0, n = 0, rlen = strlen(word); i < rlen; i++)
-            {
-
-                char junk = ptr -> spell[i - n];
-                char junk1 = junk - 32;
-
-                // checks characters
-                if(junk == word[i] || junk1 == word[i])
-                    equals += 1;
-
-                // chekcs apostrophes usage
-                else if(word[i] == 39)
-                    ++n;
-
-                // breaks out
-                else
-                    break;
-            }
-
-            // return true if the words are same
-            if(equals == len)
-                return true;
-
-            // go to the next node
-            ptr = ptr -> next;
-        }
+        ptr = ptr -> next;
     }
 
     // if all else fails, return false
@@ -114,34 +77,28 @@ bool load(const char *dictionary)
 
     // open the dictionary
     FILE *filename = fopen(dictionary, "r");
-
-    // check if the file exists
     if(filename == NULL)
         return false;
 
-    //  variable declaration with the size of the largest string in the dictionary
     char correct[LENGTH + 1];
-
-    // create temp nodes
     struct node *array;
 
     while (fscanf(filename, "%s", correct) != EOF)
     {
-        // find the alphabets position
-        int ht_number = correct[0] - 97;
+        // find the hashtable
+        int ht_number = hashit(correct[0]);
 
-        // allocate memory and check if the fuction returns NULL
+        // sanity check
         array = malloc(sizeof(words));
         if(array == NULL)
+        {
+            unload();
+            fclose(filename);
             return false;
+        }
 
-        // copy string
         strcpy(array -> spell, correct);
-
-        // assaign pointer
         array -> next = ht[ht_number];
-
-        // current address
         ht[ht_number] = array;
     }
 
@@ -157,23 +114,18 @@ bool load(const char *dictionary)
  */
 unsigned int size(void)
 {
-    // temp variables
     int n = 0;
 
-    // interate through the words
     for(int i = 0; i < 24; i++)
     {
         struct node *ptr = ht[i];
 
-        // increment n by 1
         while(ptr != NULL)
         {
             n++;
             ptr = ptr -> next;
         }
     }
-
-    // returns the total words
     return n;
 }
 
