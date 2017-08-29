dac996421eee0a297efc2db5258378a287496d6d
Ajay Katte <ajaymkatte95@gmail.com>
2017-08-29 13:10:29 +0000

    Analyzer words :)

--- pset6/sentiments/analyzer.py
+++ pset6/sentiments/analyzer.py
@@ -6,10 +6,30 @@ class Analyzer():
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
 
-        # TODO
+        self.good, self.bad = set(), set()
+
+        # Open file for each file
+        goodfile = open(positives, "r")
+        badfile = open(negatives, "r")
+
+        # Init the sets
+        for line in goodfile:
+            if line[0] != ';' and line[0] != '\n':
+                self.good.add(line.rstrip("\n"))
+
+        for line in badfile:
+            if line[0] != ';' and line[0] != '\n':
+                self.bad.add(line.rstrip("\n"))
 
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
 
-        # TODO
-        return 0
+        # Calculate score
+        if text.lower() in self.good:
+            score = 1
+        elif text.lower() in self.bad:
+            score = -1
+        else:
+            score = 0
+
+        return score
