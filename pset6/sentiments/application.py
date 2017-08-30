8291813a878ad8c3ac02ff731ad24a4a855be3ae
Ajay Katte <ajaymkatte95@gmail.com>
2017-08-30 11:27:05 +0000

    Removed file usage. application.py works

--- pset6/sentiments/application.py
+++ pset6/sentiments/application.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect, render_template, request, url_for
 
+import os
+import sys
 import helpers
+import nltk
 from analyzer import Analyzer
 
 app = Flask(__name__)
@@ -12,16 +15,41 @@ def index():
 @app.route("/search")
 def search():
 
+    # absolute paths to lists
+    positives = os.path.join(sys.path[0], "positive-words.txt")
+    negatives = os.path.join(sys.path[0], "negative-words.txt")
+
+    # instantiate analyzer
+    analyzer = Analyzer(positives, negatives)
+
     # validate screen_name
     screen_name = request.args.get("screen_name", "")
     if not screen_name:
         return redirect(url_for("index"))
 
     # get screen_name's tweets
-    tweets = helpers.get_user_timeline(screen_name)
+    tweets = (helpers.get_user_timeline(screen_name, 100))
 
-    # TODO
-    positive, negative, neutral = 0.0, 0.0, 100.0
+    count, positive, negative, neutral = 0, 0, 0, 0
+
+    # Break into strings
+    for element in tweets:
+        count += 1
+        score = 0
+        tokenizer = nltk.tokenize.TweetTokenizer()
+        tokens = tokenizer.tokenize(element)
+        for word in tokens:
+            score += analyzer.analyze(word)
+        if score > 0:
+            positive += 1
+        elif score < 0:
+            negative += 1
+        elif score == 0:
+            neutral +=1
+
+    positive = (100 * positive) / count
+    negative = (100 * negative) / count
+    neutral = (100 * neutral) / count
 
     # generate chart
     chart = helpers.chart(positive, negative, neutral)
